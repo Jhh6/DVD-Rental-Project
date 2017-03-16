6a5e395169f25c7948f0c97db6fca4605c4c20ce
Jacob Homanics <jhh6@students.ptcollege.edu>
2017-03-16 14:58:09 -0400

    NEW FOLDER
    It works

---
+++ DVDRental/Project/Project/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Movie
+/// </summary>
+/// 
+namespace Project
+{
+    public class Movie
+    {
+        public Movie()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+
+}
